ea80ad0e4c8a95fa2302eadfb871fb98ab8c13ab
“Cynthia <xin2604@gmail.com>
2022-05-02 16:53:16 -0700

    implement add name feature

--- frontend/src/components/NameList.js
+++ frontend/src/components/NameList.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const NameList = (props) => {
   const [nameList, setLists] = useState([]);
+  const [first, setF] = useState('');
+  const [last, setL] = useState('')
   const getNameList = () => {
     axios.get('http://localhost:3001/api/name')
       .then(res => {
@@ -12,13 +14,36 @@ const NameList = (props) => {
   useEffect(() => {
     getNameList()
   })
+  const addName = () => {
+    if(first.length && last.length) {
+      axios.post('http://localhost:3001/api/name', {firstName: first, lastName: last})
+        .then(res => {
+        console.log('success add a name')
+      })
+        .catch(err => console.log(err))
+
+    } else {
+      alert('Please enter full name')
+    }
+  }
   return (
     <>
       <p>Display Name List: </p>
       {nameList.map((name, index) =>
         <p key={index}>{name.firstName + ' ' + name.lastName}</p>
       )}
-      <button className="add">Add a Name</button>
+      <form>
+        <div>
+          <label>Enter first name</label>
+          <input type="text" name="fn" required onChange={(e) => setF(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Enter last name</label>
+          <input type="text" name="ln" required onChange={(e) => setL(e.target.value)}></input>
+        </div>
+      </form>
+      <br></br>
+      <button className="add" onClick={addName}>Add a Name</button>
     </>
   )
 }
