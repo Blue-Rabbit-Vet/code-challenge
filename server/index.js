452b6d8a7d1449f59d2eb3c53217667673a886ee
“Cynthia <xin2604@gmail.com>
2022-05-02 15:43:42 -0700

    install cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = require('./router.js')
 const db = require('../database/db.js');
+const cors = require('cors');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 app.use('/api', router);
 
-const port = 3000;
+const port = 3001;
 app.listen(port, (err) => {
   err ? console.log(err) : console.log(`server listening on port ${port}`);
 })
