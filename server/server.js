bd14b53e2791303c3d531a319a67e41ccfb3ae8e
meshtatsuo <meshtatsuo@gmail.com>
2022-06-19 14:41:57 -0600

    helps if I direct root route to the right page!

--- server/server.js
+++ server/server.js
@@ -20,6 +20,9 @@ app.use(routes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 sequelize
   .sync({
