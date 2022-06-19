93be764eac9700349b34233160b0b410137b79cf
meshtatsuo <meshtatsuo@gmail.com>
2022-06-19 14:53:29 -0600

    should now be loading the react app properly

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,17 @@
 const router = require("express").Router();
 
+const path = require("path");
+
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+router.use((req, res) => {
+  res.status(404).end();
 });
 
 module.exports = router;
